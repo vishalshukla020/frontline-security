dc4bbdcf2f36ebe0a6879ec41cbffeca5dbf9a94
vishal shukla <shuklavishal737@gmail.com>
2021-07-31 15:41:11 +0530

    import

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,5 @@
 import "../sass/style.scss";
 import Layout from "../components/Layout";
-const isCI = require("is-ci");
-
-if (isCI) {
-  console.log("The code is running on a CI server");
-}
 
 function MyApp({ Component, pageProps }) {
   return (
